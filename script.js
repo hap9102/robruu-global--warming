2cbbce29d1a1dff5a18dce72d12df51f8ea3a8bb
hap9102 <114100301+hap9102@users.noreply.github.com>
2023-02-28 23:13:23 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -2,6 +2,9 @@
 const slideshow = document.querySelector(".slideshow");
 const images = slideshow.querySelectorAll("#img");
 
+const links = document.querySelectorAll(".link");
+const bodyId = document.querySelector("body").id;
+
 //causes
 
 let index = 0;
@@ -11,3 +14,17 @@ setInterval(() => {
   index = (index + 1) % images.length;
   images[index].classList.add(`active`);
 }, 3000);
+
+const currentPath = window.location.pathname;
+// loop through all menu items
+const menuItems = document.querySelectorAll(".link");
+
+menuItems.forEach((menuItem) => {
+  // get the href attribute of the menu item
+
+  const href = menuItem.getAttribute("href").slice(1);
+  // compare the href attribute with the current page path
+  if (href === currentPath) {
+    menuItem.classList.add("activeLink");
+  }
+});
